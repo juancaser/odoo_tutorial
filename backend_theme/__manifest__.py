d6e2118ed480a2bbc2ae88fa41f9009bf03d8655
Juan <jan.caser@korlanda.com>
2019-02-01 10:43:11 +0800

    Added backend theme tutorial

---
+++ backend_theme/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Juan Caser
+
+{
+    "name": "Backend Theme Tutorial",
+    "summary": "Odoo 12 Backend Theme",
+    "version": "12.0.0.1",
+    "category": "Theme/Backend",
+    "website": "https://www.facebook.com/groups/odoodevph",
+	"description": """
+		Backend theme for Odoo 12.0 CE.
+    """,
+	'images':[],
+    "author": "Juan Caser",
+    "license": "LGPL-3",
+    "installable": True,
+    "data": [
+        'views/assets.xml'
+    ]
+}
+
