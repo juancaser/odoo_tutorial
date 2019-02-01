55e48d401b5882276e353af99293d6b4a4a59806
Juan <jan.caser@korlanda.com>
2019-02-01 10:42:43 +0800

    Added basic module tutorial

---
+++ basic_module/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Juan Caser
+
+{
+    "name": "Basic Module Tutorial",
+    "summary": "Odoo 12 Basic Module",
+    "version": "12.0.0.1",
+    "category": "Hidden",
+    "website": "https://www.facebook.com/groups/odoodevph",
+	"description": """
+		Basic module for Odoo 12.0 CE.
+    """,
+	'images':[],
+    "author": "Juan Caser",
+    "license": "LGPL-3",
+    "installable": True,
+    "data": []
+}
+
